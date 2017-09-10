b1dd16a0458460f77a7a2237d781c6916e141102
Tobias Lidskog <tobias@lidskog.se>
2017-09-10 22:18:55 +0200

    Bump dev dependencies and reformat source.

--- check-latest.js
+++ check-latest.js
@@ -4,20 +4,19 @@
 const request = require('request');
 const currentVersion = require('./package.json').chromedriver_version;
 
-request('https://chromedriver.storage.googleapis.com/LATEST_RELEASE', function(
-  error,
-  response,
-  body
-) {
-  if (error) {
-    console.log('Failed to parse latest release version: ' + error.message);
-    return process.exit(1);
-  }
+request(
+  'https://chromedriver.storage.googleapis.com/LATEST_RELEASE',
+  (error, response, body) => {
+    if (error) {
+      console.log(`Failed to parse latest release version: ${error.message}`);
+      return process.exit(1);
+    }
 
-  const latestVersion = Number(body.trim());
-  if (latestVersion > Number(currentVersion)) {
-    console.log(`Upgrade to ${latestVersion}`);
-    process.exit(1);
+    const latestVersion = Number(body.trim());
+    if (latestVersion > Number(currentVersion)) {
+      console.log(`Upgrade to ${latestVersion}`);
+      process.exit(1);
+    }
+    console.log(`Relax, ${currentVersion} is the latest version`);
   }
-  console.log(`Relax, ${currentVersion} is the latest version`);
-});
+);
